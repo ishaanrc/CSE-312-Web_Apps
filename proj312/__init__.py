5eb592b079225fd4986f04df87ca5811ad125498
Dannen Roberts <dantheman5398@gmail.com>
2020-03-12 19:15:58 -0400

    Docker mostly working, for some reason can't connect outside of the container

--- proj312/__init__.py
+++ proj312/__init__.py
@@ -8,6 +8,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
 
 
+
     if test_config is None:
         # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
